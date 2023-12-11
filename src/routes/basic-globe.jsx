f66ed708222373b7b10e42513b7531ae7e57e6a8
Paul Scanlon <pauliescanlon@gmail.com>
2023-12-11 10:13:45 -0600

    feat: add basic globe example

--- src/routes/basic-globe.jsx
+++ src/routes/basic-globe.jsx
@@ -1,7 +1,30 @@
+import Globe from 'react-globe.gl';
+
 const BasicGlobe = () => {
+  const myData = [
+    {
+      lat: 29.953204744601763,
+      lng: -90.08925929478903,
+      altitude: '0.4',
+      color: '#00ff33',
+    },
+    {
+      lat: 28.621322361013092,
+      lng: 77.20347613099612,
+      altitude: '0.4',
+      color: '#ff0000',
+    },
+    {
+      lat: -43.1571459086602,
+      lng: 172.72338919659848,
+      altitude: '0.4',
+      color: '#ffff00',
+    },
+  ];
+
   return (
-    <div>
-      <h2>Basic Globe</h2>
+    <div className='cursor-move'>
+      <Globe globeImageUrl='/earth-night.jpg' pointsData={myData} pointAltitude='altitude' pointColor='color' />
     </div>
   );
 };
