9279d8463f4cbb1f1eadba3e80ab2782ae7f58bc
Paul Scanlon <pauliescanlon@gmail.com>
2023-12-11 10:05:57 -0600

    feat: add tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
