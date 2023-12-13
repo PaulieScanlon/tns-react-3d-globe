71ea14e505ab3725b9cb25e291bc5ae03704695c
Paul Scanlon <pauliescanlon@gmail.com>
2023-12-13 12:17:04 -0600

    feat: add links to all routes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,29 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <NavLink to='/basic-image'>/basic-image</NavLink>
+      <ul className='hidden'>
+        <li>
+          <NavLink to='/basic-image'>/basic-image</NavLink>
+        </li>
+        <li>
+          <NavLink to='/geojson-polygon'>/geojson-polygon</NavLink>
+        </li>
+        <li>
+          <NavLink to='/geojson-hexagon'>/geojson-hexagon</NavLink>
+        </li>
+        <li>
+          <NavLink to='/arcs-data'>/arcs-data</NavLink>
+        </li>
+        <li>
+          <NavLink to='/rings-data'>/rings-data</NavLink>
+        </li>
+        <li>
+          <NavLink to='/html-marker'>/html-marker</NavLink>
+        </li>
+        <li>
+          <NavLink to='/custom-layer'>/custom-layer</NavLink>
+        </li>
+      </ul>
       <App />
     </BrowserRouter>
   </React.StrictMode>
